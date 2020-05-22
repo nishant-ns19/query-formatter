5c7d16f11c97d54b3164bc9826246c368bcccd4f
Nishant <nishant.jai@outlook.com>
2020-05-22 15:36:57 +0530

    Initial Commit

--- extension.js
+++ extension.js
@@ -15,6 +15,7 @@ function activate(context)
 		var editor=vscode.window.activeTextEditor;
 		if (editor)
 		{
+			//Retrieve current document
 			const document = editor.document;
 			if(!document)
 			{
@@ -22,26 +23,31 @@ function activate(context)
 			}
 			else
 			{
+				//Get selecton object
 				const selection = editor.selection;
+				//Get selected text and if nothing is selected, everything is retrieved
 				const textSelection =selection.isEmpty ? document.getText() : document.getText(selection);
 				if(!textSelection)
-			{
-				vscode.window.showInformationMessage("Please provide some text");
-			}
-			else
-			{
-				console.log("Indenting Lucene Query..." );
-				vscode.window.setStatusBarMessage("Indenting Lucene Query...",1000)
-				try
 				{
-					var result=format(textSelection);
-					if(result!==textSelection)
+					vscode.window.showInformationMessage("Please provide some text");
+				}
+				else
+				{
+					console.log("Indenting Lucene Query..." );
+					vscode.window.setStatusBarMessage("Indenting Lucene Query...",1000)
+					try
 					{
-						editor.edit(builder => builder.replace(selection.isEmpty
-							? new vscode.Range(new vscode.Position(0,0),new vscode.Position(document.lineCount,0)) 
-							:selection,result)).then(success =>{
+						//Formet the query
+						var result=format(textSelection);
+						if(result!==textSelection)
+						{
+							//Use editor object to replace the text
+							editor.edit(builder => builder.replace(selection.isEmpty
+								? new vscode.Range(new vscode.Position(0,0),new vscode.Position(document.lineCount,0))
+								:selection,result)).then(success =>{
 								console.log("Indented successfully: "+success);
 								vscode.window.showInformationMessage("Indented successfully !");
+								//If nothing was selected by the user,deselect everything my moving cursor to the end after replacing th text
 								if(selection.isEmpty)
 								{
 									var postion = editor.selection.end; 
@@ -79,97 +85,100 @@ module.exports = {
 */
 function format(query)	
 {
+	//Omit spaces around the query
 	query=removeSpace(query)
-	if(query.charAt(0)=='\"' && query.charAt(query.length-1)=='\"' && query.length>=2)
-	{	query=query.substr(1,query.length-2);
+	//Remove "" around the query
+	if(query.charAt(0)=='\"' && query.charAt(query.length-1)=='\"' && query.length>=2)	
+	{	
+		query=query.substr(1,query.length-2);
 	}
 	var result="";
-	var countTabs=0;
+	var tabCount=0;
+	//inPhrase signifies if we are traversing the quoted text
 	var inPhrase=false;
 	for (let idx = 0; idx < query.length; idx++)
 	{
+		//toggle inPhrase when " is encountered
 		if(query.charAt(idx)=='\"')
 		{
+			//toggle inPhrase
 			inPhrase=!inPhrase;
 			result=result+query.charAt(idx);
+			//Jump onto new line after completing each phrase(quoted text)
 			if(!inPhrase)
 			{
 				result=result+"\n";
-				for(let i=0;i<countTabs;i++)
-				{
-					result=result+"\t";
-				}
+				result=result+"\t".repeat(tabCount);
 			}
 			continue;
 		}
+		//quoted text should be printed as it is
 		if(inPhrase)
 		{
 			result=result+query.charAt(idx);
+			//in case quoted text contains newline character, cursor should move onto next block but should not change the current block
 			if(query.charAt(idx)=='\n')
 			{
-				for(let i=0;i<countTabs;i++)
-				{	
-					result=result+"\t";
-				}
+				result=result+"\t".repeat(tabCount);
 			}
 			continue;
 		}
+		//Omit exisiting spacing characters from the string
 		if(query.charAt(idx)==' ' || query.charAt(idx)=='\t' || query.charAt(idx)=='\n' || query.charAt(idx)=='\r')
 		{
 			continue;
 		}
+		//Handling ',' separately
 		if(query.charAt(idx)==',')
 		{
+			//Incase cursor is at the starting of a new line, add tab spacing to get into the current block
 			if(result.charAt(result.length-1)=='\n')
 			{
-				for(let i=0;i<countTabs;i++)
-				{
-					result=result+"\t";
-				}
+				result=result+"\t".repeat(tabCount);
 			}
+			//append ','
 			result=result+query.charAt(idx);
+			//Jump onto new line and respective indent block after each ','
 			result=result+"\n";
-			for(let i=0;i<countTabs;i++)
-			{
-				result=result+"\t";
-			}
+			result=result+"\t".repeat(tabCount);
 		}
+		//Start a new block whenever an opening paranthesis is encountered
 		else if(query.charAt(idx)=='(' || query.charAt(idx)=='[' || query.charAt(idx)=='{')
 		{
 			result=result+query.charAt(idx);
+			//Jump onto new line
 			result=result+"\n";
-			countTabs++;
-			for(let i=0;i<countTabs;i++)
-			{
-				result=result+"\t";
-			}
+			//Add a new block by increasing number of tabs
+			tabCount++;
+			result=result+"\t".repeat(tabCount);
 		}
+		//End the current block whenever a closing paranthesis is encountered
 		else if(query.charAt(idx)==')' || query.charAt(idx)==']' || query.charAt(idx)=='}')
 		{
+			//If the cursor has already moved onto the next line, remove '\t' from end as cursor needs to go back to the previous block
 			if(result.charAt(result.length-1)=='\t')
 			{
 				result=result.substr(0,result.length-1);
 			}
+			//otherwise, move onto the next line, get onto the preivous block
 			else
 			{
 				result=result+"\n";
-				for(let i=0;i<countTabs-1;i++)
-				{
-					result=result+"\t";
-				}
+				result=result+"\t".repeat(tabCount-1);
 			}
-			countTabs--;
+			// decrement tabCount due to closing of the block
+			tabCount--;
 			result=result+query.charAt(idx);
+			//incase there is a ',' after closing a block, it has to be printed just after closing so continue without moving onto the next line
 			if((idx<(query.length-1)) && query.charAt(idx+1)==',')
 			{
 				continue;
 			}
+			//otherwise, move onto the next line, jump onto the current block
 			result=result+"\n";
-			for(let i=0;i<countTabs;i++)
-			{
-				result=result+"\t";
-			}
+			result=result+"\t".repeat(tabCount);
 		}
+		//in any other case, just print the character
 		else
 		{
 			result=result+query.charAt(idx);
@@ -184,10 +193,12 @@ function format(query)
 function removeSpace(query)
 {
 	var l=0,r=query.length-1
+	//Remove space characters from starting
 	while(l<query.length && (query[l]==' ' || query[l]=='\r' || query[l]=='\t' || query[l]=='\n'))
 	{
 		l++;
 	}
+	//Remove space characters from end
 	while(r>=0 && (query[r]==' ' || query[r]=='\r' || query[r]=='\t' || query[r]=='\n'))
 	{
 		r--;
