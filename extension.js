719830cfb71aa1668db244da9f485c884b1d9bb7
Nishant <nishant.jai@outlook.com>
2020-06-03 16:59:20 +0530

    Show error via notification

--- extension.js
+++ extension.js
@@ -28,7 +28,7 @@ function activate(context) {
           if (!textSelection) {
             window.showInformationMessage("Please provide some text");
           } else {
-            console.log("Indenting Lucene Query...");
+            console.log("Indenting Lucene Query..");
             window.setStatusBarMessage("Indenting Lucene Query...", 1000);
             try {
               //format query
@@ -59,7 +59,7 @@ function activate(context) {
               }
             } catch (error) {
               console.log("Error occurred: " + error);
-              window.showInformationMessage("Unable to process text !");
+              window.showInformationMessage("Unable to process text !:\n" + error);
             }
           }
         }
